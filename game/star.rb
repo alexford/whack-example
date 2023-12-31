e46ce5dec622b664fabd8013c43ba26def61f507
Alex Ford <alex@cratercreative.com>
2023-12-31 14:32:31 -0500

    Move Star to its own file

---
+++ game/star.rb
@@ -0,0 +1,49 @@
+class Game
+  class Star
+    MIN_LIFETIME = 5.0
+    MAX_LIFETIME = 30.0
+
+    attr_reader :remaining_lifetime
+
+    def initialize(x, y, size)
+      @x = x
+      @y = y
+      @size = size
+      @remaining_lifetime = rand(MIN_LIFETIME..MAX_LIFETIME)
+    end
+
+    def move(dt, max_x, max_y)
+      @remaining_lifetime = [remaining_lifetime - dt, 0].max
+
+      if (remaining_lifetime < 1.0)
+        @size = [@size - remaining_lifetime, 0].max
+      end
+
+      @x += 1
+      @y += 1
+
+      @y += Math.sin(@x / 200.0 + (dt / 30.0)) * @size
+      @x += Math.cos(@y / 100.0 + (dt / 30.0)) * @size
+
+      if @x >= max_x
+        @x = 0
+      end
+
+      if @y >= max_y
+        @y = 0
+      end
+
+      if @y < 0
+        @y = max_y
+      end
+
+      if @x < 0
+        @x = max_x
+      end
+    end
+
+    def draw
+      Whack::Objects::RectangleVectors.new(@x, @y, @size, @size).draw
+    end
+  end
+end
